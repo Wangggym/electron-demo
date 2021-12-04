cb130a4ff402145ba393c2bb8fd41b587ba445bd
wangym <wangym0203@aliyun.com>
2021-12-04 18:18:39 +0800

    feat: config boundle electron

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const { app, BrowserWindow } = require("electron");
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
@@ -10,4 +15,11 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
